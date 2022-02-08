ea2a9d6d140810a156b4300a62156deeb68fb421
Pawel Ciebiera <pawelciebiera@gmail.com>
2022-02-08 20:44:19 +0100

    Add Variable if/else

--- js/script.js
+++ js/script.js
@@ -14,3 +14,19 @@ printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wy
 var playerMove;
 playerMove = 'papier';
 printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to kamień , to przegrywasz!')
+
+
+
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} 
+if (randomNumber == '2') {
+    computerMove = 'papier';
+  }
+if (randomNumber == '3') {
+    computerMove = 'nożyce';
+  }
+printMessage('Mój ruch: ' + computerMove);
