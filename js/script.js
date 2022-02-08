08f5c51ea6f9e2a0413d81be46b0322e2502ccb1
Pawel Ciebiera <pawelciebiera@gmail.com>
2022-02-08 20:05:19 +0100

    add first script

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function printMessage(msg){
+	var div = document.createElement('div');
+	div.innerHTML = msg;
+	document.getElementById('messages').appendChild(div);
+}
+
+function clearMessages(){
+	document.getElementById('messages').innerHTML = '';
+}
+var computerMove;
+computerMove = 'kamień';
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+
+var playerMove;
+playerMove = 'papier';
+printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to kamień , to przegrywasz!')
